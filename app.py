643f961163db62abb5985daaf64c9a09c960aa44
Books-Master18 <osipovasasha11@yandex.ru>
2026-02-08 15:51:17 +0300

    Trigger new deploy

--- app.py
+++ app.py
@@ -131,6 +131,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(
     __name__,
+    
     static_folder=os.path.join(BASE_DIR, 'Static'),
     template_folder=os.path.join(BASE_DIR, 'templates')
 )
